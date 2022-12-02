384eb43cc7419303fc32c00b774d8a4c4067c1dc
sagar Gidwani <sagar Gidwani@Sagar>
2022-12-02 09:57:49 +0530

    First commit, all files uploaded

---
+++ sagar/src/sagarr/dre.java
@@ -0,0 +1,59 @@
+package sagarr;
+
+import java.util.Scanner;
+
+public class dre {
+
+	private static final String krishna = null;
+
+	public static void main(String[] args) {
+		//System.out.println("hello");
+		// TODO Auto-generated method stub
+	//	Scanner sc = new Scanner(System.in);
+		//System.out.println("enter number of subjects");
+		/*int noOfSubject = sc.nextInt();
+		System.out.println(noOfSubject);
+		System.out.println("enter your maths marks");
+        float maths = sc.nextFloat();
+        System.out.println("english marks");
+        float english = sc.nextFloat();
+        System.out.println("hindi marks");
+        float hindi = sc.nextFloat();
+        
+        float total = maths + english + hindi;
+        float percent = total/noOfSubject ;
+        System.out.println("the total percentage of student is "  +   percent);*/
+		
+		/*char grade = 'B';
+		grade = (char)(grade +8);
+		System.out.println(grade);
+		
+		//Encrypt the code
+		grade = (char)(grade-8);
+		System.out.println(grade);
+        
+        	*/
+		
+		
+
+		/*	int a = 34;
+		Scanner sc =new Scanner(System.in);
+		System.out.print("enter the value of b:   ");
+		int b = sc.nextInt();
+		
+		System.out.println(a>b);
+				
+
+*/
+
+		String name = "krishna     ";
+		
+				
+		name = name.replace( " ","_");
+		
+		System.out.println(name);
+				
+				
+	}
+
+}
